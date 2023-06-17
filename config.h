c1958fc277187988f2589b2a8a64c6ce047d5e3a
tragdate <tragdate@gmail.com>
2023-06-17 23:34:26 +0300

    ffclip - open clipboard url in default browser

--- config.h
+++ config.h
@@ -283,7 +283,7 @@ static Key keys[] = {
         {MODKEY | ShiftMask, XK_Print, spawn, {.v = (const char *[]) {"dmenurecord", "kill", NULL}}},
         {MODKEY, XK_Delete, spawn, {.v = (const char *[]) {"dmenurecord", "kill", NULL}}},
         {MODKEY, XK_Scroll_Lock, spawn, SHCMD("killall screenkey || screenkey &")},
-
+        {ShiftMask, XK_Home, spawn, {.v = (const char *[]) {"ffclip", NULL}}},
         {0, XF86XK_AudioMute, spawn, SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)")},
         {0, XF86XK_AudioRaiseVolume, spawn, SHCMD("pamixer --allow-boost -i 3; kill -44 $(pidof dwmblocks)")},
         {0, XF86XK_AudioLowerVolume, spawn, SHCMD("pamixer --allow-boost -d 3; kill -44 $(pidof dwmblocks)")},
